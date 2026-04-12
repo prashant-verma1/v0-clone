f5cbdcd8e4fbc84b9e919db5d86e95a4f1fb547a
[Prashant Verma] <diamandg5726@gmail.com>
2026-04-12 08:47:41 +0530

    feat: implement navbar, theme provider, and global styles with Clerk authentication integration

---
+++ module/home/components/navbar.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import Logo from "./logo.jsx";
+import { Button } from "@/components/ui/button.jsx";
+import { ModeToggle } from "@/components/ui/toggle-mode.jsx";
+import {
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+
+const Navbar = () => {
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
+
+  return (
+    <nav className="p-4 fixed top-0 left-0 right-0 z-50">
+      <div className="max-w-5xl mx-auto w-full flex justify-between items-center">
+
+        <Logo />
+        <ModeToggle />
+
+        {!isSignedIn && (
+          <div className="flex gap-2">
+            <SignInButton mode="modal">
+              <Button variant="outline" size="sm">Sign In</Button>
+            </SignInButton>
+
+            <SignUpButton mode="modal">
+              <Button size="sm">Sign Up</Button>
+            </SignUpButton>
+          </div>
+        )}
+
+        {isSignedIn && <UserButton />}
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
