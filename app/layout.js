aa78828ed5c42a188f32f9ebe3773a002787a454
[Prashant Verma] <diamandg5726@gmail.com>
2026-04-14 20:02:48 +0530

    hero section done

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,22 +22,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html
-        lang="en"
-        suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <body className="min-h-full flex flex-col">
-          <ThemeProvider
+        <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
+
+            <Toaster/>
+        {children}
+        </ThemeProvider>
+      </body>
+    </html>
     </ClerkProvider>
   );
 }
